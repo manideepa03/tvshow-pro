4880dc3d2ccce46c0ffbac68af246a84c09397dc
manideepa03 <mani.deepa30@gmail.com>
2020-09-28 17:01:19 -0700

    Fixed _error file

--- pages/_error.js
+++ pages/_error.js
@@ -8,21 +8,21 @@
 // export const getServerSideProps = async (err, res) => {
 //   return { statusCode: res ? res.statusCode : err ? err.statusCode : 404 };
 // };
+// // export default CustomError;
+// const CustomError = ({ statusCode }) => {
+//   if (statusCode === 404) {
+//     return <h1>The resource was not found...</h1>;
+//   }
+
+//   return <h1>Oops! Something went wrong...</h1>;
+// };
+
+// CustomError.getServerSideProps = ({ err, res }) => {
+//   return {
+//     props: {
+//       statusCode: 404,
+//     },
+//   };
+// };
+
 // export default CustomError;
-const CustomError = ({ statusCode }) => {
-  if (statusCode === 404) {
-    return <h1>The resource was not found...</h1>;
-  }
-
-  return <h1>Oops! Something went wrong...</h1>;
-};
-
-export const getServerSideProps = ({ err, res }) => {
-  return {
-    props: {
-      statusCode: 404,
-    },
-  };
-};
-
-export default CustomError;
